1f91fe6297e7d00f9966bd971b6a08d9f8c26384
SvetlanaYarullina <yarullina6277@mail.ru>
2022-02-02 23:57:04 +0300

    add edits

---
+++ src/js/swiper.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// import Swiper from 'swiper';
+// import "../../node_modules/swiper/swiper-bundle.min";
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+});
+
+// // // init Swiper:
+// // const swiper = new Swiper('.swiper', {
+// //     // configure Swiper to use modules
+// //     modules: [Navigation, Pagination]
+// //   });
