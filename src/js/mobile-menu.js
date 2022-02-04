9c383ea0b5ccdabd2cce37c3ca85e4e3fb1b579c
SvetlanaYarullina <yarullina6277@mail.ru>
2022-02-04 23:58:34 +0300

    add mobile menu burger

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,1 +1,11 @@
 "use strict";
+
+var iconMenu = document.querySelector('.menu__icon');
+var menuBody = document.querySelector ('.menu__body');
+if (iconMenu) { 
+    iconMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('__lock');
+        iconMenu.classList.toggle('menu__icon--active');
+        menuBody.classList.toggle('menu__body--active');
+    });
+}
