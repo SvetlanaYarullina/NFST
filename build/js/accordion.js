8527bde4fb3cf5390f86d143d0aef03c12c6a548
SvetlanaYarullina <yarullina6277@mail.ru>
2022-02-08 19:21:39 +0300

    start block footer and edits

--- build/js/accordion.js
+++ build/js/accordion.js
@@ -1,17 +1,14 @@
-"use strict"; // var accordionItem = document.querySelector('.accordion__item');
-// var accordionTrigger = document.querySelector('.accordion__trigger');
-// var accordionContent = document.querySelector('.accordion__content');
-// accordionItem.addEventListener('click', function () {
-//     accordionTrigger.classList.toggle('accordion__trigger--active');
-//     accordionContent.classList.toggle('accordion__content--active');
-// });
+"use strict";
 
 document.querySelectorAll('.accordion__trigger').forEach(function (item) {
   return item.addEventListener('click', function () {
     var parent = item.parentNode;
     parent.classList.toggle('accordion__item--active');
   });
-}); // var accordionTrigger = document.querySelector('.accordion__item');
-// accordionTrigger.addEventListener('click', function () {
-//     accordionTrigger.classList.toggle('accordion__item--active');
+}); // var accordionItem = document.querySelectorAll('.accordion__item');
+// var accordionTrigger = document.querySelectorAll('.accordion__trigger');
+// var accordionContent = document.querySelectorAll('.accordion__content');
+// accordionItem.addEventListener('click', function () {
+//     accordionTrigger.classList.toggle('accordion__trigger--active');
+//     accordionContent.classList.toggle('accordion__content--active');
 // });
--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,15 +1,5 @@
 "use strict";
 
-// var accordionItem = document.querySelector('.accordion__item');
-// var accordionTrigger = document.querySelector('.accordion__trigger');
-// var accordionContent = document.querySelector('.accordion__content');
-
-// accordionItem.addEventListener('click', function () {
-//     accordionTrigger.classList.toggle('accordion__trigger--active');
-//     accordionContent.classList.toggle('accordion__content--active');
-// });
-
-
 document.querySelectorAll('.accordion__trigger').forEach((item) =>
     item.addEventListener('click', () => {
         const parent = item.parentNode;
@@ -17,10 +7,14 @@ document.querySelectorAll('.accordion__trigger').forEach((item) =>
     })
     )
 
+// var accordionItem = document.querySelectorAll('.accordion__item');
+// var accordionTrigger = document.querySelectorAll('.accordion__trigger');
+// var accordionContent = document.querySelectorAll('.accordion__content');
 
-
-
-// var accordionTrigger = document.querySelector('.accordion__item');
-// accordionTrigger.addEventListener('click', function () {
-//     accordionTrigger.classList.toggle('accordion__item--active');
+// accordionItem.addEventListener('click', function () {
+//     accordionTrigger.classList.toggle('accordion__trigger--active');
+//     accordionContent.classList.toggle('accordion__content--active');
 // });
+
+
+
