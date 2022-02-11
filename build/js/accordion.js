7b336602002e9b7e103fb4b5e05bb5b0fe6cc77d
SvetlanaYarullina <yarullina6277@mail.ru>
2022-02-11 16:48:22 +0300

    edits after review part 1

--- build/js/accordion.js
+++ build/js/accordion.js
@@ -1,14 +1,13 @@
-"use strict";
+"use strict"; // document.querySelectorAll('.accordion__trigger').forEach((item) =>
+//     item.addEventListener('click', () => {
+//         const parent = item.parentNode;
+//         parent.classList.toggle('accordion__item--active');
+//     })
+//     )
 
-document.querySelectorAll('.accordion__trigger').forEach(function (item) {
+document.querySelectorAll('[data-accordion-trigger]').forEach(function (item) {
   return item.addEventListener('click', function () {
     var parent = item.parentNode;
     parent.classList.toggle('accordion__item--active');
   });
-}); // var accordionItem = document.querySelectorAll('.accordion__item');
-// var accordionTrigger = document.querySelectorAll('.accordion__trigger');
-// var accordionContent = document.querySelectorAll('.accordion__content');
-// accordionItem.addEventListener('click', function () {
-//     accordionTrigger.classList.toggle('accordion__trigger--active');
-//     accordionContent.classList.toggle('accordion__content--active');
-// });
+});
--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,20 +1,15 @@
 "use strict";
 
-document.querySelectorAll('.accordion__trigger').forEach((item) =>
+// document.querySelectorAll('.accordion__trigger').forEach((item) =>
+//     item.addEventListener('click', () => {
+//         const parent = item.parentNode;
+//         parent.classList.toggle('accordion__item--active');
+//     })
+//     )
+
+    document.querySelectorAll('[data-accordion-trigger]').forEach((item) =>
     item.addEventListener('click', () => {
         const parent = item.parentNode;
         parent.classList.toggle('accordion__item--active');
     })
     )
-
-// var accordionItem = document.querySelectorAll('.accordion__item');
-// var accordionTrigger = document.querySelectorAll('.accordion__trigger');
-// var accordionContent = document.querySelectorAll('.accordion__content');
-
-// accordionItem.addEventListener('click', function () {
-//     accordionTrigger.classList.toggle('accordion__trigger--active');
-//     accordionContent.classList.toggle('accordion__content--active');
-// });
-
-
-
