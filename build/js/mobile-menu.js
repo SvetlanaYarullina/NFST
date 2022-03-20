ba27633dbf4bb4bc521f9ce298a57d9a10dce3dd
SvetlanaYarullina <yarullina6277@mail.ru>
2022-03-21 01:52:00 +0300

    add menu wrapper

--- build/js/mobile-menu.js
+++ build/js/mobile-menu.js
@@ -2,11 +2,23 @@
 
 var toggleMenu = document.querySelector('[data-menu-toggle]');
 var menuBody = document.querySelector('[data-menu-body]');
+var menuWrapper = document.querySelector('[data-menu-wrapper]');
 
 if (toggleMenu) {
   toggleMenu.addEventListener('click', function (e) {
     document.body.classList.toggle('__lock');
     toggleMenu.classList.toggle('menu__toggle--active');
     menuBody.classList.toggle('menu__body--active');
+    menuWrapper.classList.toggle('menu__wrapper--active');
   });
 }
+
+menuWrapper.addEventListener('click', function (e) {
+  if (!e.target.closest('.menu__body')) {
+    console.log('Клик');
+    document.body.classList.remove('__lock');
+    toggleMenu.classList.remove('menu__toggle--active');
+    menuBody.classList.remove('menu__body--active');
+    menuWrapper.classList.remove('menu__wrapper--active');
+  }
+});
